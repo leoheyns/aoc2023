29cae5cd18324b982110a6bdb59e79cb00758720
leo <leo@iris-advies.com>
2023-12-01 06:46:16 +0100

    day1 solved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use chrono::{FixedOffset, Utc, Datelike};
+use std::env;
+#[path = "day1/day1.rs"]
+mod day1;
+
+fn run_day(day: u32){
+    match day{
+        1=>day1::run(),
+        _=>println!("Soepkip! die dag bestaat niet",)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let timezone = FixedOffset::west_opt(5*3600).unwrap();
+    let now = Utc::now();
+    let datetime = now.with_timezone(&timezone);
+    let mut day = datetime.date_naive().day();
+
+    
+    if args.len() == 2{
+        day = args[1].parse::<u32>().unwrap()
+    }
+    if day < 25 && day > 0{
+        run_day(day);
+    }
 }
