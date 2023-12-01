66e75e89eff90998c32a3b1edf7f282078f82ea3
leo <leo@iris-advies.com>
2023-12-01 07:43:28 +0100

    fixed my original idea, not sure which is uglier

---
+++ src/day1/day1_alt.rs
@@ -0,0 +1,35 @@
+fn getnum(chars: std::str::Chars<'_>) -> u32{
+    let mut found = false;
+    let mut firstnum: char = '0';
+    let mut lastnum: char = '0';
+    for c in chars{
+        if !found && c.is_digit(10){
+            found = true;
+            firstnum = c;
+        }
+
+        if c.is_digit(10){
+            lastnum = c;
+        }
+    }
+
+    return firstnum.to_digit(10).unwrap() * 10 + lastnum.to_digit(10).unwrap()
+}
+
+pub fn run(){
+    let input: u32 = include_str!("input")
+    .replace("one", "one1one")
+    .replace("two", "two2two")
+    .replace("three", "three3three")
+    .replace("four", "four4four")
+    .replace("five", "five5five")
+    .replace("six", "six6six")
+    .replace("seven", "seven7seven")
+    .replace("eight", "eitht8eight")
+    .replace("nine", "nine9nine")
+
+    .lines().map(|l| l.chars())
+    .map(|cs: std::str::Chars<'_>| getnum(cs)).sum();
+
+    println!("{}", input);
+}
